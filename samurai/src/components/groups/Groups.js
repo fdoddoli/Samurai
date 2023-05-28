eba189369ab64b90a7e1a45849187ee110c2d721
Fernando Doddoli <a00827038@itesm.mx>
2023-05-27 21:29:53 -0600

    add bottom navbar

---
+++ samurai/src/components/groups/Groups.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Groups = (props) => {
+    const {auth, profile} = props;
+    
+
+    return(
+        <div>
+            <h1>hello</h1>
+        </div> 
+    );
+}
+
+
+
+export default Groups;
